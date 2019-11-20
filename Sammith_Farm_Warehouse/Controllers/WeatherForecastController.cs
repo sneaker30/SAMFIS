d7af7b94cfb7c68c9d622fb9266ca98c9c0b87c5
sneaker30.net <sneaker30.net@gmail.com>
2019-11-20 15:53:08 +0700

    test 2

--- Sammith_Farm_Warehouse/Controllers/WeatherForecastController.cs
+++ Sammith_Farm_Warehouse/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Sammith_Farm_Warehouse.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Super Hot"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
@@ -27,7 +27,7 @@ namespace Sammith_Farm_Warehouse.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 10).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
